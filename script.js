d1abff470adcd53c209eada38c9928fd7eeab804
maycondefarias <mayconfariascordeiro337@gmail.com>
2024-12-04 09:55:54 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
 
-        function searchProducts() {
-            const query = document.getElementById("search-bar").value.toLowerCase();
-            const items = document.querySelectorAll("#product-list li");
-
-            items.forEach(item => {
-                const text = item.textContent.toLowerCase();
-                if (text.includes(query)) {
-                    item.classList.remove("hidden");
-                } else {
-                    item.classList.add("hidden");
-                }
-            });
-        }
+       
